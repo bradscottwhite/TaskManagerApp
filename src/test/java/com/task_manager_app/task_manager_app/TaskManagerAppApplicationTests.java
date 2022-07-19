4f58a8cd350198c889a92f6de467d5be692d08b3
Brad White <bradscottwhite@gmail.com>
2022-07-18 22:33:31 -0400

    first commit

---
+++ src/test/java/com/task_manager_app/task_manager_app/TaskManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task_manager_app.task_manager_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
